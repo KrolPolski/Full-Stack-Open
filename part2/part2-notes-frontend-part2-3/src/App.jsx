0636c13498bb00048adbf7389b5c7f445e563573
Ryan Boudwin <ryanboudwin@outlook.com>
2025-05-05 16:42:33 +0300

    separated axiom to a service

--- part2/part2-notes-frontend-part2-3/src/App.jsx
+++ part2/part2-notes-frontend-part2-3/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -17,32 +18,41 @@ const App = () => {
 	  })
   }
   
-  useEffect(hook, [])
-  console.log('render', notes.length, 'notes')
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
   
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() > 0.5,
+      important: Math.random() > 0.5
     }
-    axios.post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-    })
-  }
+
+    noteService
+      .create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+  } 
   
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
-  const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
+  const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important}
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n => n.id == id ? response.data : n))
-    }) 
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id === id ? response.data : note))
+      })
   }
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
