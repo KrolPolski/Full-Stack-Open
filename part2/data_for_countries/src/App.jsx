663be31b60bc4fb34798e2304a30ae8ae49a4d2a
Ryan Boudwin <rboudwin@student.hive.fi>
2025-05-23 11:44:14 +0300

    Search functionality filtering now

--- part2/data_for_countries/src/App.jsx
+++ part2/data_for_countries/src/App.jsx
@@ -1,29 +1,42 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
+//mport "./App.css";
 import getCountriesService from "./services/getCountries.js";
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [allCountries, setAllCountries] = useState([]);
+  const [matchCountries, setMatchCountries] = useState ([]);
 
   useEffect(() => {
     getCountriesService.getAll().then((initialData) => {
       setAllCountries(initialData);
     });
   }, []);
-
-  const handleSearchChange = ({value}) => {
-	setSearchTerm(newSearchTerm)
-  };
+  let newSearchTerm = ""
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm)
+    const filtered = allCountries.filter(country => {
+      const lowerCountryName = country.name.common.toLowerCase();
+      const lowerSearch = newSearchTerm.toLowerCase();
+      console.log("lowerCountryName: ", lowerCountryName, "lowerSearch: ", lowerSearch)
+      console.log(lowerCountryName.startsWith(lowerSearch));
+      return lowerCountryName.startsWith(lowerSearch)});
+      setMatchCountries(filtered);
+    }
+  const PrintCountries = () => {
+    return matchCountries.map((country) => (
+        <div key={country.cca3}>{country.name.common}</div>
+      ))
+  }
   return (
     <>
-      {allCountries.map((country) => (
-        <div key={country.cca3}>{country.name.common}</div>
-      ))}
       <label>Find Countries:</label>
       <input value={searchTerm} onChange={handleSearchChange} />
+      <PrintCountries/>
     </>
   );
-}
+};
 
 export default App;
